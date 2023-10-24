ccc275fdb0419a97569b64da1d4f5f9930c9f337
cerdelen <cedric.erdelen@gmail.com>
2023-10-24 14:27:16 +0200

    backup some sort of visual ui

--- advanced_core/RNCP6_projects/task_master/src/main.rs
+++ advanced_core/RNCP6_projects/task_master/src/main.rs
@@ -3,7 +3,7 @@ use std::io::Stdout;
 use config::ConfigError;
 use my_config::ProcessGroupStruct;
 use terminal::check_for_events;
-use tui::{Terminal, backend::CrosstermBackend};
+use tui::{backend::CrosstermBackend, Terminal};
 
 mod my_config;
 use crate::my_config::TaskConfig;
@@ -11,47 +11,48 @@ use crate::my_config::TaskConfig;
 mod terminal;
 
 fn build_process_groups(config: &TaskConfig) -> Vec<ProcessGroupStruct> {
-	let mut process_groups: Vec<ProcessGroupStruct> = vec![];
-	process_groups.reserve(config.applications.len());
-	for app in &config.applications {
-		process_groups.push(app.build_command());
-	}
-	process_groups
+    let mut process_groups: Vec<ProcessGroupStruct> = vec![];
+    process_groups.reserve(config.applications.len());
+    for app in &config.applications {
+        process_groups.push(app.build_command());
+    }
+    process_groups
 }
 
 fn launch_groups(groups: &mut Vec<ProcessGroupStruct>) {
-	for group in groups {
-		while group.vec_of_single_processes.len() < group.conf.amt {
-		    group.launch_one_process();
-		}
-	}
+    for group in groups {
+        while group.vec_of_single_processes.len() < group.conf.amt {
+            group.launch_one_process();
+        }
+    }
 }
 
 pub struct AllData {
-	curr_conf:		TaskConfig,
-	term:			Terminal<CrosstermBackend<Stdout>>,	
-	display:		terminal::GUIState,
+    _curr_conf: TaskConfig,
+    term: Terminal<CrosstermBackend<Stdout>>,
+    display: terminal::GUIState,
 }
 
-fn main() -> Result<(), ConfigError>{
-	let mut term: tui::Terminal<tui::backend::CrosstermBackend<std::io::Stdout>> = terminal::build_terminal();
+fn main() -> Result<(), ConfigError> {
+    let term: tui::Terminal<tui::backend::CrosstermBackend<std::io::Stdout>> =
+        terminal::build_terminal();
 
-	let config: TaskConfig = TaskConfig::build()?;
+    let config: TaskConfig = TaskConfig::build()?;
 
-	let mut process_groups = build_process_groups(&config);
+    let mut process_groups = build_process_groups(&config);
 
-	launch_groups(&mut process_groups);
+    launch_groups(&mut process_groups);
 
-	let mut state = AllData {
-		curr_conf: config,
-		term,
-		display: terminal::GUIState{cursor: (0,0)},
-	};
+    let mut state = AllData {
+        _curr_conf: config,
+        term,
+        display: terminal::GUIState { cursor: (0, 0) },
+    };
 
-	terminal::draw_term(&mut state);
+    terminal::draw_term(&mut state);
 
-	let _ = check_for_events(&mut state);
-	// println!("{:?}!", process_groups[0].vec_of_single_processes[0].child);
-	// println!("end of main");
-	Ok(())
+    let _ = check_for_events(&mut state);
+    // println!("{:?}!", process_groups[0].vec_of_single_processes[0].child);
+    // println!("end of main");
+    Ok(())
 }
--- advanced_core/RNCP6_projects/task_master/src/terminal/mod.rs
+++ advanced_core/RNCP6_projects/task_master/src/terminal/mod.rs
@@ -1,71 +1,72 @@
-use crossterm::{terminal::{enable_raw_mode, disable_raw_mode}, event::{KeyEvent, KeyCode}};
-use tui::{ layout::{Constraint, Direction, Layout}, widgets::{Block, Borders, Wrap, Paragraph}, text::{Text, Span, Spans}, style::{Color, Style, Modifier}, };
-use std::{io, time::Duration, process::exit, cell::Cell, fmt::Alignment};
+use crossterm::{
+    event::{KeyCode, KeyEvent},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use std::{io, process::exit, time::Duration};
 use tui::{backend::CrosstermBackend, Terminal};
+use tui::{
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Borders, Paragraph, Wrap},
+};
 
-use crate::{AllData, terminal};
+use crate::{terminal, AllData};
 
 use crossterm::event::{poll, read, Event};
 
 pub fn build_terminal() -> Terminal<CrosstermBackend<io::Stdout>> {
-	let stdout = io::stdout();
+    let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
-	let mut terminal = Terminal::new(backend).expect("Terminal is not opening!");
-	let _ = tui::Terminal::clear(&mut terminal);
-	return terminal;
+    let mut terminal = Terminal::new(backend).expect("Terminal is not opening!");
+    let _ = tui::Terminal::clear(&mut terminal);
+    terminal
 }
 
 pub struct GUIState {
-	pub cursor: (u16, u16),
+    pub cursor: (u16, u16),
 }
 
-fn key_press(key_event: KeyEvent, data: &mut AllData)
-{
-	// println!("{:?}", key_event);
-	if key_event == KeyEvent::from(terminal::KeyCode::Char('q')) {
-		let _ = disable_raw_mode();
-		exit(0);
-	}
-	if key_event == KeyEvent::from(terminal::KeyCode::Char('h')) {
-		if data.display.cursor.0 > u16::MIN {
-			data.display.cursor.0 -= 1;
-		}
-	}
-	if key_event == KeyEvent::from(terminal::KeyCode::Char('j')) {
-		if data.display.cursor.1 < u16::MAX {
-			data.display.cursor.1 += 1;
-		}
-	}
-	if key_event == KeyEvent::from(terminal::KeyCode::Char('k')) {
-		if data.display.cursor.1 > u16::MIN {
-			data.display.cursor.1 -= 1;
-		}
-	}
-	if key_event == KeyEvent::from(terminal::KeyCode::Char('l')) {
-		if data.display.cursor.0 < u16::MAX {
-			data.display.cursor.0 += 1;
-		}
-	}
-	if key_event == KeyEvent::from(terminal::KeyCode::Char('1')) {
-		data.display.cursor.0 = 0;
-		data.display.cursor.1 = 0;
-	}
-	data.term.draw(|f| {
-		f.set_cursor(data.display.cursor.0, data.display.cursor.1);
-    }).expect("error of draw term");
+fn key_press(key_event: KeyEvent, data: &mut AllData) {
+    // println!("{:?}", key_event);
+    if key_event == KeyEvent::from(terminal::KeyCode::Char('q')) {
+        let _ = disable_raw_mode();
+        exit(0);
+    }
+    if key_event == KeyEvent::from(terminal::KeyCode::Char('h')) && data.display.cursor.0 > u16::MIN
+    {
+        data.display.cursor.0 -= 1;
+    }
+    if key_event == KeyEvent::from(terminal::KeyCode::Char('j')) && data.display.cursor.1 < u16::MAX
+    {
+        data.display.cursor.1 += 1;
+    }
+    if key_event == KeyEvent::from(terminal::KeyCode::Char('k')) && data.display.cursor.1 > u16::MIN
+    {
+        data.display.cursor.1 -= 1;
+    }
+    if key_event == KeyEvent::from(terminal::KeyCode::Char('l')) && data.display.cursor.0 < u16::MAX
+    {
+        data.display.cursor.0 += 1;
+    }
+    if key_event == KeyEvent::from(terminal::KeyCode::Char('1')) {
+        data.display.cursor.0 = 0;
+        data.display.cursor.1 = 0;
+    }
 }
 
 pub fn check_for_events(data: &mut AllData) -> io::Result<()> {
-	let _ = enable_raw_mode();
-	loop {
+    let _ = enable_raw_mode();
+    loop {
         if poll(Duration::from_millis(500))? {
             match read()? {
                 Event::Key(event) => key_press(event, data),
-				_ => panic!("smth else happened"),
-				// Event::FocusGained => println!("FocusGained"),
-				// Event::FocusLost => println!("FocusLost"),
-    //             Event::Resize(width, height) => println!("New size {}x{}", width, height),
+                _ => panic!("smth else happened"),
+                // Event::FocusGained => println!("FocusGained"),
+                // Event::FocusLost => println!("FocusLost"),
+                //             Event::Resize(width, height) => println!("New size {}x{}", width, height),
             }
+            draw_term(data);
         } else {
             // Timeout expired and no `Event` is available
         }
@@ -74,41 +75,35 @@ pub fn check_for_events(data: &mut AllData) -> io::Result<()> {
 }
 
 pub fn draw_term(data: &mut AllData) {
-	enable_raw_mode().expect("raw mode cannot be enabled!");
-	data.term.draw(|f| {
-		let chunks = Layout::default()
-			.direction(Direction::Vertical)
-			.margin(1)
-			.constraints(
-				[
-					Constraint::Percentage(80),
-					Constraint::Percentage(20),
-				].as_ref()
-			)
-			.split(f.size());
+    enable_raw_mode().expect("raw mode cannot be enabled!");
+    data.term
+        .draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
+                .split(f.size());
 
-        let block = Block::default()
-            .title("Status")
-            .borders(Borders::ALL);
-		let block2 = Block::default()
-			.title("Help")
-            .borders(Borders::ALL);
-		let text = vec![
-			Spans::from(vec![
-			Span::raw("First"),
-			Span::styled("line",Style::default().add_modifier(Modifier::ITALIC)),
-			Span::raw("."),
-			]),
-			Spans::from(Span::styled("Second line", Style::default().fg(Color::Red))),
-		];
-		let par = Paragraph::new(text)
-			.block(Block::default().title("Paragraph").borders(Borders::ALL))
-			.style(Style::default().fg(Color::White).bg(Color::Black))
-			// .alignment(Alignment::Center)
-			.wrap(Wrap { trim: true });
-        f.render_widget(block, chunks[0]);
-        f.render_widget(block2, chunks[1]);
-		f.render_widget(par , chunks[0]);
-    }).expect("error of draw term");
-	disable_raw_mode().expect("raw mode not enabled!");
+            let block = Block::default().title("Status").borders(Borders::ALL);
+            let block2 = Block::default().title("Help").borders(Borders::ALL);
+            let text = vec![
+                Spans::from(vec![
+                    Span::raw("First"),
+                    Span::styled("line", Style::default().add_modifier(Modifier::ITALIC)),
+                    Span::raw("."),
+                ]),
+                Spans::from(Span::styled("Second line", Style::default().fg(Color::Red))),
+            ];
+            let par = Paragraph::new(text)
+                .block(Block::default().title("Paragraph").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White).bg(Color::Black))
+                // .alignment(Alignment::Center)
+                .wrap(Wrap { trim: true });
+            f.render_widget(block, chunks[0]);
+            f.render_widget(block2, chunks[1]);
+            f.render_widget(par, chunks[0]);
+            f.set_cursor(data.display.cursor.0, data.display.cursor.1);
+        })
+        .expect("error of draw term");
+    // disable_raw_mode().expect("raw mode not enabled!");
 }
